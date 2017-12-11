0727223670e66b53d54cfdafb94ff9e1b31fe427
Ryo Nakamura <upa@haeena.net>
2017-12-12 02:31:12 +0900

    ppktgen: add udp/raw socket i/o mode. (rx is not tested)

--- tools/ppktgen.c
+++ tools/ppktgen.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <string.h>
 #include <signal.h>
 #include <sched.h>
@@ -16,13 +17,19 @@
 #include <sys/uio.h>
 #include <pthread.h>
 #include <linux/if_ether.h>
+#include <linux/if_packet.h>
+#include <net/if.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <netinet/udp.h>
 #include <arpa/inet.h>
 
 
-#define pr_info(fmt, ...) fprintf(stdout, "%s: " fmt, __func__, ##__VA_ARGS__)
+#define pr_info(fmt, ...) fprintf(stdout, "%s: " fmt, \
+				  __func__, ##__VA_ARGS__)
+#define pr_warn(fmt, ...) fprintf(stdout, "\x1b[1m\x1b[31m"	\
+				  "%s:WARN: " fmt "\x1b[0m",	\
+				  __func__, ##__VA_ARGS__)
 #define pr_err(fmt, ...) fprintf(stderr, "%s: " fmt, __func__, ##__VA_ARGS__)
 
 
@@ -35,6 +42,10 @@
 #define UDP_DST_PORT	60000
 #define UDP_SRC_PORT	60001
 
+/* ppktgen i/o mode */
+#define IO_MODE_HPIO	1
+#define IO_MODE_RAW	2
+#define IO_MODE_UDP	3
 
 
 /* ppktgen thread structure */
@@ -70,6 +81,11 @@ struct ppktgen_body {
 	unsigned short	udp_dst;	/* udp dst port */
 	unsigned short	udp_src;	/* udp src port */
 
+	int io_mode;	/* packet i/o mode */
+
+	bool rx_mode;	/* RX mode if true */
+	bool per_cpu_rx;	/* per CPU mode for Raw and UDP RX */
+
 	int ncpus;	/* number of cpus */
 	int nthreads;	/* number of threads */
 
@@ -88,7 +104,7 @@ struct ppktgen_body {
 
 /* global variable */
 static int caught_signal = 0;
-
+static int hpio_fd = 0;
 
 
 /* from netmap pkt-gen.c */
@@ -155,13 +171,158 @@ void build_tx_packet(struct ppktgen_body *pbody, struct ppktgen_thread *pt)
 
 	/* build udp header */
 	udp = (struct udphdr *)(ip + 1);
-	udp->uh_ulen	= htons(pbody->len - sizeof(*eth) - sizeof(*ip));
+	udp->uh_ulen	= htons(pbody->len -
+				sizeof(*eth) - sizeof(*ip));
 	udp->uh_dport	= pbody->udp_dst;
-	udp->uh_sport	= ((pbody->udp_src + pt->cpu) * 0x61C88647) & 0xffff;
+	udp->uh_sport	= ((pbody->udp_src + pt->cpu) *
+			   0x61C88647) & 0xffff;
 	/* XXX: magical value from include/linux/hash.h for RSS */
-
+	
 }
 
+
+
+/* socket retrieve */
+
+int get_hpio_socket(char *hpio_devpath)
+{
+	if (hpio_fd == 0)  {
+		/* open hpio device first. */
+		hpio_fd = open(hpio_devpath, O_RDWR);
+		if (hpio_fd < 0) {
+			pr_err("cannot open device %s\n", hpio_devpath);
+			perror("open");
+			return -1;
+		}
+	}
+	return hpio_fd;
+}
+
+int get_raw_socket(int cpu, bool per_cpu_rx, char *dev)
+{
+	int fd, ret;
+	socklen_t len;
+	struct sockaddr_ll sll;
+
+	fd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+	if (fd < 0) {
+		pr_err("cannot create raw socket\n");
+		perror("socket");
+		return -1;
+	}
+	
+	memset(&sll, 0, sizeof(sll));
+	sll.sll_family = AF_PACKET;
+	sll.sll_protocol = htons(ETH_P_IP);
+	sll.sll_ifindex = if_nametoindex(dev);
+	
+	ret = bind(fd, (struct sockaddr *)&sll, sizeof(sll));
+	if (ret < 0) {
+		pr_err("failed to bind raw socket to %s\n", dev);
+		perror("bind");
+		return -1;
+	}
+
+	if (per_cpu_rx) {
+		len = sizeof(cpu);
+		ret = setsockopt(fd, SOL_SOCKET, SO_INCOMING_CPU, &cpu, len);
+		if (ret < 0) {
+			pr_err("failed to setsockopt SO_INCOMING_CPU\n");
+			perror("setsockopt");
+			return -1;
+		}
+	}
+
+	return fd;
+}
+
+int get_udp_socket(int cpu, bool per_cpu_rx, bool rx_mode,
+		   struct in_addr dst, int port)
+{
+	int fd, ret;
+	socklen_t len;
+	struct sockaddr_in saddr;
+
+	fd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (fd < 0) {
+		pr_err("cannot create udp socket\n");
+		perror("socket");
+		return -1;
+	}
+
+	if (rx_mode) {
+		/* bind for RX. Not bind for TX, because of RSS */
+		memset(&saddr, 0, sizeof(saddr));
+		saddr.sin_family = AF_INET;
+		saddr.sin_port = htons(port);
+		saddr.sin_addr.s_addr = INADDR_ANY;
+		ret = bind(fd, (struct sockaddr *)&saddr, sizeof(saddr));
+		if (ret < 0) {
+			pr_err("failed to bind udp socket\n");
+			perror("bind");
+			return -1;
+		}
+	} else {
+		/* connect for TX. */
+		memset(&saddr, 0, sizeof(saddr));
+		saddr.sin_family = AF_INET;
+		saddr.sin_port = htons(port);
+		saddr.sin_addr = dst;
+		ret = connect(fd, (struct sockaddr *)&saddr, sizeof(saddr));
+		if (ret < 0) {
+			pr_err("failed to connect\n");
+			perror("connect");
+			return -1;
+		}
+	}
+
+
+	if (per_cpu_rx) {
+		len = sizeof(cpu);
+		ret = setsockopt(fd, SOL_SOCKET, SO_INCOMING_CPU, &cpu, len);
+		if (ret < 0) {
+			pr_err("failed to setsockopt SO_INCOMING_CPU\n");
+			perror("setsockopt");
+			return -1;
+		}
+	}
+
+	return fd;
+}
+	
+int get_socket_fd(struct ppktgen_body *pb, struct ppktgen_thread *pt)
+{
+	int fd = -1, n;
+	char *dev = NULL;
+
+	switch(pb->io_mode) {
+	case IO_MODE_HPIO :
+		fd = get_hpio_socket(pb->devpath);
+		break;
+
+	case IO_MODE_RAW :
+
+		/* find device name from hpio device path */
+		for (n = strlen(pb->devpath); n > 0; n--) {
+			if (pb->devpath[n] == '/') {
+				dev = &(pb->devpath[n + 1]);
+				break;
+			}
+		}
+		fd = get_raw_socket(pt->cpu, pb->per_cpu_rx, dev);
+		break;
+
+	case IO_MODE_UDP :
+		fd = get_udp_socket(pt->cpu, pb->per_cpu_rx, pb->rx_mode,
+				    pb->dst_ip, UDP_DST_PORT);
+		break;
+	default :
+		pr_err("invalid i/o mode %d\n", pb->io_mode);
+		return -1;
+	}
+	
+	return fd;
+}
 
 
 /* ppktgen tx thread body on a cpu */
@@ -182,13 +343,34 @@ void * ppktgen_tx_thread(void *arg)
 	pt->slot.hdr.version = HPIO_HDR_VERSION;
 	pt->slot.hdr.hdrlen = sizeof(struct hpio_hdr);
 	pt->slot.hdr.pktlen = pbody->len;
-	build_tx_packet(pbody, pt);
+
+	if (pbody->io_mode != IO_MODE_UDP) {
+		build_tx_packet(pbody, pt);
+	}		
 
 	/* initialize packet iovec buffer */
-	for (n = 0; n < pbody->bulk; n++) {
-		iov[n].iov_base = &pt->slot;	/* fill the ptr to the slot */
-		iov[n].iov_len = pbody->len + sizeof(struct hpio_hdr);
+	switch (pbody->io_mode) {
+	case IO_MODE_HPIO :
+		for (n = 0; n < pbody->bulk; n++) {
+			/* fill the ptr to the slot */
+			iov[n].iov_base = &pt->slot;
+			iov[n].iov_len = pbody->len + sizeof(struct hpio_hdr);
+		}
+		break;
+
+	case IO_MODE_RAW :
+		iov[0].iov_base = &pt->slot.pkt;
+		iov[0].iov_len = pbody->len;
+		break;
+
+	case IO_MODE_UDP :
+		iov[0].iov_base = &pt->slot.pkt;
+		iov[0].iov_len = pbody->len - (sizeof(struct ethhdr) +
+					       sizeof(struct ip) +
+					       sizeof(struct udphdr));
+		break;
 	}
+
 
 	/* write packets */
 	while (1) {
@@ -196,12 +378,17 @@ void * ppktgen_tx_thread(void *arg)
 			break;
 
 		cnt = writev(pt->fd, iov, pbody->bulk);
+
 		if (cnt < 0) {
 			pr_err("writev() failed on cpu %d\n", pt->cpu);
+			perror("writev");
 			exit (EXIT_FAILURE);
 		}
 
-		pt->pkt_count += cnt;
+		if (pbody->io_mode == IO_MODE_HPIO)
+			pt->pkt_count += cnt;
+		else
+			pt->pkt_count += 1;
 
 		if (pt->count) {
 			pt->count--;
@@ -322,13 +509,16 @@ void usage(void)
 	printf("ppktgen usage:\n"
 	       "\t -i: path to hpio device\n"
 	       "\t -r: rx mode (rx at all CPU)(default is tx)\n"
+	       "\t -m: packet i/o mode (hpio|raw|udp) default hpio\n"
+	       "\t -p: per CPU mode for raw/udp sockets: SO_INCOMING_CPU,"
+	       "(on|off) default on\n"
 	       "\t -a: print pps stat on each CPU\n"
 	       "\t -d: destination IPv4 address\n"
 	       "\t -s: source IPv4 address\n"
 	       "\t -D: destination MAC address\n"
 	       "\t -S: source MAC address\n"
 	       "\t -l: length of a packet\n"
-	       "\t -m: CPU mask (hex)\n"
+	       "\t -M: CPU mask (hex)\n"
 	       "\t -n: number of threads\n"
 	       "\t -b: number of bulked packets\n"
 	       "\t -c: number of executing writev() on each cpu\n"
@@ -339,8 +529,9 @@ void usage(void)
 int main(int argc, char **argv)
 {
 
-	int fd, ch, n, cpu, rc, rx_mode = 0;
+	int ch, n, cpu, rc;
 	int dmacbuf[ETH_ALEN], smacbuf[ETH_ALEN];
+	char *io_mode_str = "hpio";
 	char buf[16];		/* for printing parameters to stdout */
 	pthread_t pkt_count_tid;	/* pthread id for pkt count thread */
 	unsigned int use_cpu = 0, mask;
@@ -350,14 +541,18 @@ int main(int argc, char **argv)
 	memset(smacbuf, 0, sizeof(smacbuf));
 
 	memset(&ppktgen, 0, sizeof(ppktgen));
+	ppktgen.rx_mode = false;
 	ppktgen.ncpus = count_online_cpus();
 	ppktgen.nthreads = 1;
 	ppktgen.bulk = 1;
 	ppktgen.len = 60;
 	ppktgen.udp_dst = htons(UDP_DST_PORT);
 	ppktgen.udp_src = htons(UDP_SRC_PORT);
+	ppktgen.io_mode = IO_MODE_HPIO;
+	ppktgen.per_cpu_rx = true;
 
-	while ((ch = getopt(argc, argv, "i:rad:s:D:S:l:m:n:b:c:t:")) != -1) {
+	while ((ch = getopt(argc, argv, "i:rm:p:ad:s:D:S:l:M:n:b:c:t:"))
+	       != -1) {
 		switch (ch) {
 		case 'i' :
 			/* hpio device path */
@@ -365,9 +560,34 @@ int main(int argc, char **argv)
 			break;
 
 		case 'r' :
-			rx_mode = 1;
+			ppktgen.rx_mode = true;
 			break;
 
+		case 'm' :
+			if (strncmp(optarg, "hpio", 4) == 0)
+				ppktgen.io_mode = IO_MODE_HPIO;
+			else if (strncmp(optarg, "raw", 3) == 0) {
+				ppktgen.io_mode = IO_MODE_RAW;
+				io_mode_str = "raw";
+			}
+			else if (strncmp(optarg, "udp", 3) == 0) {
+				ppktgen.io_mode = IO_MODE_UDP;
+				io_mode_str = "udp";
+			}else {
+				pr_err("invalid i/o mode '%s'\n", optarg);
+				return -1;
+			}
+			break;
+		case 'p' :
+			if (strncmp(optarg, "on", 2) == 0)
+				ppktgen.per_cpu_rx = true;
+			else if (strncmp(optarg, "off", 3) == 0)
+				ppktgen.per_cpu_rx = false;
+			else {
+				pr_err("invalid per_cpu_rx mode '%s'", optarg);
+				return -1;
+			}
+			break;
 		case 'a' :
 			ppktgen.print_all_cpu_pps = 1;
 			break;
@@ -426,7 +646,7 @@ int main(int argc, char **argv)
 			}
 			break;
 
-		case 'm' :
+		case 'M' :
 			/* CPU mask to specify CPUs to run threads */
 			rc = sscanf(optarg, "%x", &use_cpu);
 			if (rc < 1) {
@@ -480,18 +700,27 @@ int main(int argc, char **argv)
 		}
 	}
 
-	if (rx_mode) {
+	if (ppktgen.rx_mode) {
 		/* In RX mode, ppktgen receives packets on all CPUs
-		 * because hpio (currently )does not support all
+		 * because hpio (currently ) does not support all
 		 * packets to specified CPU(s).
 		 */
+		pr_warn("When RX mode, ppktgen uses all CPUs\n");
 		ppktgen.nthreads = ppktgen.ncpus;
+	}
+
+	if (ppktgen.io_mode == IO_MODE_RAW || ppktgen.io_mode == IO_MODE_UDP) {
+		pr_warn("When raw|udp io mode, bluk size is always 1\n");
+		ppktgen.bulk = 1;
 	}
 
 
 	/* print parameters */
 	pr_info("============ Parameters ============\n");
 	pr_info("dev:               %s\n", ppktgen.devpath);
+	pr_info("io_mode            %s\n", io_mode_str);
+	pr_info("rx_mode            %s\n", (ppktgen.rx_mode) ? "yes" : "no");
+	pr_info("per_cpu_rx         %s\n", (ppktgen.per_cpu_rx) ? "on" :"off");
 
 	inet_ntop(AF_INET, &ppktgen.dst_ip, buf, sizeof(buf));
 	pr_info("dst IP:            %s\n", buf);
@@ -516,14 +745,6 @@ int main(int argc, char **argv)
 	pr_info("====================================\n");
 
 
-	/* open hpio fd */
-	fd = open(ppktgen.devpath, O_RDWR);
-	if (fd < 0) {
-		pr_err("cannot open device %s\n", ppktgen.devpath);
-		perror("open");
-		return -1;
-	}
-
 	/* create threads */
 	mask = 1;
 	for (n = 0; n < ppktgen.nthreads; n++) {
@@ -547,16 +768,16 @@ int main(int argc, char **argv)
 			cpu = n;
 		}
 
-		ppktgen.pt[n].fd = fd;
 		ppktgen.pt[n].thn = n;
 		ppktgen.pt[n].cpu = cpu;
 		ppktgen.pt[n].pbody = &ppktgen;
 		ppktgen.pt[n].count = ppktgen.count;
+		ppktgen.pt[n].fd = get_socket_fd(&ppktgen, &ppktgen.pt[n]);
 
 		pr_info("Create thread %d on cpu %d\n",
 			ppktgen.pt[n].thn, ppktgen.pt[n].cpu);
 
-		if (!rx_mode) {
+		if (!ppktgen.rx_mode) {
 			rc = pthread_create(&ppktgen.pt[n].tid, NULL,
 					    ppktgen_tx_thread, &ppktgen.pt[n]);
 		} else {
@@ -591,8 +812,6 @@ int main(int argc, char **argv)
 
 
 	pthread_join(pkt_count_tid, NULL);
-
-	close (fd);
 
 	return 0;
 }
